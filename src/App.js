0ceb860b0ede5d641cfaa90407713923d0b06153
Celena Ponce <138083273+celenaaponce@users.noreply.github.com>
2024-06-30 16:00:32 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import './App.css';
-import React, { useRef, useEffect, useState } from "react";
-import Webcam from "react-webcam";
+import React, { useRef, useEffect } from "react";
 import { Holistic } from "@mediapipe/holistic";
-import * as cam from "@mediapipe/camera_utils";
 import * as HolisticModule from "@mediapipe/holistic";
 
 function App() {
   const [xDistance, setXDistance] = useState(null);
   const [yDistance, setYDistance] = useState(null);
   const webcamRef = useRef(null);
-  const canvasRef = useRef(null);
-  const cameraRef = useRef(null);  // Use useRef for the camera
-  const connect = window.drawConnectors;
-  const drawLandmarks = window.drawLandmarks
-  
+  const cameraRef = useRef(null);
+
+  function calcBoundingRect(imageWidth, imageHeight, landmarks) {
+    let landmarkArray = [];
+
+    for (let i = 0; i < landmarks.length; i++) {
+      const landmark = landmarks[i];
+      const landmarkX = Math.min(
+        Math.floor(landmark.x * imageWidth),
+        imageWidth - 1
+      );
+      const landmarkY = Math.min(
+        Math.floor(landmark.y * imageHeight),
+        imageHeight - 1
+      );
+      landmarkArray.push({ x: landmarkX, y: landmarkY });
+    }
+
+    // Calculate bounding rectangle
+    let minX = imageWidth,
+      minY = imageHeight;
+    let maxX = 0,
+      maxY = 0;
+
+    for (let i = 0; i < landmarkArray.length; i++) {
+      const { x, y } = landmarkArray[i];
+      if (x < minX) minX = x;
+      if (x > maxX) maxX = x;
+      if (y < minY) minY = y;
+      if (y > maxY) maxY = y;
+    }
+
+    const boundingRect = {
+      x: minX,
+      y: minY,
+      width: maxX - minX,
+      height: maxY - minY,
+    };
+
+    return boundingRect;
+  }
+
   function preProcessLandmark(landmarkList) {
     // Extract x and y values from landmarkList
-    const xValues = landmarkList.map(element => element.x);
-    const yValues = landmarkList.map(element => element.y);
+    const xValues = landmarkList.map((element) => element.x);
+    const yValues = landmarkList.map((element) => element.y);
 
     // Deep copy x and y values
     const tempX = [...xValues];
@@ -40,107 +74,54 @@ function App() {
 
     // Normalization
     const maxAbsValue = Math.max(...tempLandmarkList.map(Math.abs));
-    const normalizedLandmarkList = tempLandmarkList.map(value => value / maxAbsValue);
+    const normalizedLandmarkList = tempLandmarkList.map(
+      (value) => value / maxAbsValue
+    );
 
     return normalizedLandmarkList;
   }
 
-  function calcBoundingRect(frame, landmarks, imageWidth, imageHeight) {
-  let landmarkArray = [];
-
-  for (let i = 0; i < landmarks.length; i++) {
-    const landmark = landmarks[i];
-    const landmarkX = Math.min(Math.floor(landmark.x * imageWidth), imageWidth - 1);
-    const landmarkY = Math.min(Math.floor(landmark.y * imageHeight), imageHeight - 1);
-    landmarkArray.push({ x: landmarkX, y: landmarkY });
-  }
-
-  // Calculate bounding rectangle
-  let minX = imageWidth, minY = imageHeight;
-  let maxX = 0, maxY = 0;
-
-  for (let i = 0; i < landmarkArray.length; i++) {
-    const { x, y } = landmarkArray[i];
-    if (x < minX) minX = x;
-    if (x > maxX) maxX = x;
-    if (y < minY) minY = y;
-    if (y > maxY) maxY = y;
-  }
-
-  const boundingRect = {
-    x: minX,
-    y: minY,
-    width: maxX - minX,
-    height: maxY - minY
-  };
-    console.log(boundingRect)
-
-  return boundingRect;
-}
   function onResults(results) {
-    const imageWidth = webcamRef.current.video.videoWidth;
-    const imageHeight = webcamRef.current.video.videoHeight;
-
-    // Set canvas width
-    canvasRef.current.width = imageWidth;
-    canvasRef.current.height = imageHeight;
-
-    const canvasElement = canvasRef.current;
-    const canvasCtx = canvasElement.getContext("2d");
-    canvasCtx.save();
-    canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-    canvasCtx.drawImage(
-      results.image,
-      0,
-      0,
-      canvasElement.width,
-      canvasElement.height
-    );
-
-    // Draw pose landmarks
-    if (results.poseLandmarks) {
-      connect(canvasCtx, results.poseLandmarks, HolisticModule.POSE_CONNECTIONS, { color: "#00FF00", lineWidth: 4 });
-      drawLandmarks(canvasCtx, results.poseLandmarks, HolisticModule.POSE_CONNECTIONS, { color: "#00FF00", lineWidth: 4 })
-    }
-
     if (results.poseLandmarks && results.leftHandLandmarks) {
-      const xDistance = Math.abs(results.poseLandmarks.landmark[3].x - results.leftHandLandmarks.landmark[4].x);
-      const yDistance = Math.abs(results.poseLandmarks.landmark[3].y - results.leftHandLandmarks.landmark[4].y);
+      const xDistance = Math.abs(
+        results.poseLandmarks.landmark[3].x -
+          results.leftHandLandmarks.landmark[4].x
+      );
+      const yDistance = Math.abs(
+        results.poseLandmarks.landmark[3].y -
+          results.leftHandLandmarks.landmark[4].y
+      );
       setXDistance(xDistance);
       setYDistance(yDistance);
 
-      const boundingRect = calcBoundingRect(canvasCtx, results.leftHandLandmarks, imageWidth, imageHeight);
-      // const preProcessedLandmarks = preProcessLandmark(results.leftHandLandmarks.landmark);}
+      const boundingRect = calcBoundingRect(frame, results.leftHandLandmarks);
+      const preProcessedLandmarks = preProcessLandmark(
+        results.leftHandLandmarks.landmark
+      );
       console.log(boundingRect);
-      console.log(yDistance);
-      console.log(xDistance);
-    // // Draw face landmarks
-    // if (results.faceLandmarks) {
-    //   drawConnectors(canvasCtx, results.faceLandmarks, FACEMESH_TESSELATION, { color: "#C0C0C070", lineWidth: 1 });
-    // }
-
-    // Draw left hand landmarks
-    if (results.leftHandLandmarks) {
-      connect(canvasCtx, results.leftHandLandmarks, HolisticModule.HAND_CONNECTIONS, { color: "#CC0000", lineWidth: 5 });
-      drawLandmarks(canvasCtx, results.leftHandLandmarks, HolisticModule.HAND_CONNECTIONS, { color: "#CC0000", lineWidth: 5 });
+      console.log(preProcessedLandmarks);
     }
-
-    // Draw right hand landmarks
-    if (results.rightHandLandmarks) {
-      connect(canvasCtx, results.rightHandLandmarks, HolisticModule.HAND_CONNECTIONS, { color: "#CC0000", lineWidth: 5 });
-      drawLandmarks(canvasCtx, results.rightHandLandmarks, HolisticModule.HAND_CONNECTIONS, { color: "#00CC00", lineWidth: 5 });
+    if (results.poseLandmarks) {
+      const imageWidth = webcamRef.current.video.videoWidth;
+      const imageHeight = webcamRef.current.video.videoHeight;
+      const boundingRect = calcBoundingRect(
+        imageWidth,
+        imageHeight,
+        results.poseLandmarks.landmark
+      );
+      console.log("Bounding Rectangle:", boundingRect);
+      // Use boundingRect as needed
     }
-    canvasCtx.restore();
   }
 
   useEffect(() => {
-    const holisticEffect = new Holistic({
+    const holistic = new Holistic({
       locateFile: (file) => {
         return `https://cdn.jsdelivr.net/npm/@mediapipe/holistic/${file}`;
       },
     });
 
-    holisticEffect.setOptions({
+    holistic.setOptions({
       modelComplexity: 1,
       smoothLandmarks: true,
       enableSegmentation: false,
@@ -150,19 +131,25 @@ function App() {
       minTrackingConfidence: 0.5,
     });
 
-    holisticEffect.onResults(onResults);
+    holistic.onResults(onResults);
 
     if (webcamRef.current !== null) {
-      cameraRef.current = new cam.Camera(webcamRef.current.video, {
+      cameraRef.current = new HolisticModule.Camera(webcamRef.current.video, {
         onFrame: async () => {
-          await holisticEffect.send({ image: webcamRef.current.video });
+          await holistic.send({ image: webcamRef.current.video });
         },
         width: 640,
         height: 480,
       });
       cameraRef.current.start();
     }
-  });}
+
+    return () => {
+      if (cameraRef.current) {
+        cameraRef.current.stop();
+      }
+    };
+  }, []);
 
   return (
     <div className="App">
@@ -171,36 +158,19 @@ function App() {
           <div className="App">
             <Webcam
               ref={webcamRef}
-              style={{
-                textAlign: "center",
-                zindex: 9,
-                width: '300px',
-                height: 'auto',
-                display:'none'
-              }}
-            />{" "}
+              style={{ display: "none" }} // Hide webcam if not needed for display
+            />
             <canvas
               ref={canvasRef}
               className="output_canvas"
               style={{
-                zindex: 9,
-                width: '300px',
-                height: 'auto',
+                width: "100%",
+                height: "auto",
+                zIndex: 9,
               }}
             ></canvas>
           </div>
         </center>
-        <p>
-          React JS <code>Holistic</code> using MediaPipe.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
