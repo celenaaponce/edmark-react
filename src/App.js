86a8dc3a63fd80f99883e21c891a9c73a068fe2e
Celena Ponce <138083273+celenaaponce@users.noreply.github.com>
2024-06-30 22:29:00 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,111 @@
-import "./App.css";
+import React, { useRef, useEffect } from "react";
+import { Holistic } from "@mediapipe/holistic";
+import * as cam from "@mediapipe/camera_utils";
 import CustomWebcam from "./CustomWebcam"; // import it
+import "./App.css";
 
-const App = () => {
+function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+  const connect = window.drawConnectors;
+  const drawLandmarks = window.drawLandmarks;
+  var camera = null;
+
+  function onResults(results) {
+    const videoWidth = webcamRef.current.video.videoWidth;
+    const videoHeight = webcamRef.current.video.videoHeight;
+
+    // Set canvas width
+    canvasRef.current.width = videoWidth;
+    canvasRef.current.height = videoHeight;
+
+    const canvasElement = canvasRef.current;
+    const canvasCtx = canvasElement.getContext("2d");
+    canvasCtx.save();
+    canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+    canvasCtx.drawImage(
+      results.image,
+      0,
+      0,
+      canvasElement.width,
+      canvasElement.height
+    );
+
+    // Draw pose landmarks
+    if (results.poseLandmarks) {
+      drawLandmarks(canvasCtx, results.poseLandmarks, {
+        color: "#00ff00",
+        lineWidth: 2,
+      });
+    }
+
+    canvasCtx.restore();
+  }
+
+  useEffect(() => {
+    const holistic = new Holistic({
+      locateFile: (file) => {
+        return `https://cdn.jsdelivr.net/npm/@mediapipe/holistic/${file}`;
+      },
+    });
+
+    holistic.setOptions({
+      modelComplexity: 1,
+      smoothLandmarks: true,
+      enableSegmentation: false,
+      refineFaceLandmarks: true,
+      minDetectionConfidence: 0.5,
+      minTrackingConfidence: 0.5,
+    });
+
+    holistic.onResults(onResults);
+
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null
+    ) {
+      camera = new cam.Camera(webcamRef.current.video, {
+        onFrame: async () => {
+          await holistic.send({ image: webcamRef.current.video });
+        },
+        width: 640,
+        height: 480,
+      });
+      camera.start();
+    }
+  }, []);
+
   return (
     <div className="App">
-      <CustomWebcam />
+      <header className="App-header">
+        <center>
+          <div className="App">
+            <CustomWebcam />
+            <canvas
+              ref={canvasRef}
+              className="output_canvas"
+              style={{
+                zindex: 9,
+                width: "300px",
+                height: "auto",
+              }}
+            ></canvas>
+          </div>
+        </center>
+        <p>
+          React js <code>Holistic</code> using Mediapipe.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-};
+}
 
 export default App;
