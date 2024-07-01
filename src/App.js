89bcad4352c8d0cc6c7db382743aebc36566a405
Celena Ponce <138083273+celenaaponce@users.noreply.github.com>
2024-06-30 20:16:59 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,92 @@
-import './App.css';
-import React, { useRef, useEffect } from "react";
+import "./App.css";
+import React, { useRef, useEffect, useState } from "react";
 import Webcam from "react-webcam";
 import { Holistic } from "@mediapipe/holistic";
-import * as cam from "@mediapipe/camera_utils";
 import * as HolisticModule from "@mediapipe/holistic";
 
 function App() {
+  // const [, setXDistance] = useState(null);
+  // const [, setYDistance] = useState(null);
   const webcamRef = useRef(null);
+  const cameraRef = useRef(null);
   const canvasRef = useRef(null);
-  const cameraRef = useRef(null);  // Use useRef for the camera
   const connect = window.drawConnectors;
-  // const drawLandmarks = window.drawLandmarks
+
+  // function calcBoundingRect(imageWidth, imageHeight, landmarks) {
+  //   let landmarkArray = [];
+
+  //   for (let i = 0; i < landmarks.length; i++) {
+  //     const landmark = landmarks[i];
+  //     const landmarkX = Math.min(
+  //       Math.floor(landmark.x * imageWidth),
+  //       imageWidth - 1
+  //     );
+  //     const landmarkY = Math.min(
+  //       Math.floor(landmark.y * imageHeight),
+  //       imageHeight - 1
+  //     );
+  //     landmarkArray.push({ x: landmarkX, y: landmarkY });
+  //   }
+
+  //   // Calculate bounding rectangle
+  //   let minX = imageWidth,
+  //     minY = imageHeight;
+  //   let maxX = 0,
+  //     maxY = 0;
+
+  //   for (let i = 0; i < landmarkArray.length; i++) {
+  //     const { x, y } = landmarkArray[i];
+  //     if (x < minX) minX = x;
+  //     if (x > maxX) maxX = x;
+  //     if (y < minY) minY = y;
+  //     if (y > maxY) maxY = y;
+  //   }
+
+  //   const boundingRect = {
+  //     x: minX,
+  //     y: minY,
+  //     width: maxX - minX,
+  //     height: maxY - minY,
+  //   };
+
+  //   return boundingRect;
+  // }
+
+  // function preProcessLandmark(landmarkList) {
+  //   // Extract x and y values from landmarkList
+  //   const xValues = landmarkList.map((element) => element.x);
+  //   const yValues = landmarkList.map((element) => element.y);
+
+  //   // Deep copy x and y values
+  //   const tempX = [...xValues];
+  //   const tempY = [...yValues];
+
+  //   // Convert to relative coordinates
+  //   let baseX = tempX[0];
+  //   let baseY = tempY[0];
+  //   for (let i = 0; i < tempX.length; i++) {
+  //     tempX[i] -= baseX;
+  //     tempY[i] -= baseY;
+  //   }
+
+  //   // Flatten and interleave x and y values
+  //   const tempLandmarkList = [];
+  //   for (let i = 0; i < tempX.length; i++) {
+  //     tempLandmarkList.push(tempX[i]);
+  //     tempLandmarkList.push(tempY[i]);
+  //   }
+
+  //   // Normalization
+  //   const maxAbsValue = Math.max(...tempLandmarkList.map(Math.abs));
+  //   const normalizedLandmarkList = tempLandmarkList.map(
+  //     (value) => value / maxAbsValue
+  //   );
+
+  //   return normalizedLandmarkList;
+  // }
 
   function onResults(results) {
+
     const videoWidth = webcamRef.current.video.videoWidth;
     const videoHeight = webcamRef.current.video.videoHeight;
 
@@ -31,37 +105,52 @@ function App() {
       canvasElement.width,
       canvasElement.height
     );
+    // const imageWidth = webcamRef.current.video.videoWidth;
+    // const imageHeight = webcamRef.current.video.videoHeight;
+    if (results.poseLandmarks && results.leftHandLandmarks) {
+        connect(canvasCtx, results.poseLandmarks, HolisticModule.POSE_CONNECTIONS, { color: "#00FF00", lineWidth: 4 });
 
-    // Draw pose landmarks
-    if (results.poseLandmarks) {
-      connect(canvasCtx, results.poseLandmarks, HolisticModule.POSE_CONNECTIONS, { color: "#00FF00", lineWidth: 4 });
-    }
+  //     connect(canvasCtx, results.poseLandmarks, HolisticModule.POSE_CONNECTIONS, { color: "#00FF00", lineWidth: 4 });
+  //     connect(canvasCtx, results.leftHandLandmarks, HolisticModule.HAND_CONNECTIONS, { color: "#CC0000", lineWidth: 5 });
+  //     const xDistance = Math.abs(
+  //       results.poseLandmarks.landmark[3].x -
+  //         results.leftHandLandmarks.landmark[4].x
+  //     );
+  //     const yDistance = Math.abs(
+  //       results.poseLandmarks.landmark[3].y -
+  //         results.leftHandLandmarks.landmark[4].y
+  //     );
+  //     setXDistance(xDistance);
+  //     setYDistance(yDistance);
 
-    // // Draw face landmarks
-    // if (results.faceLandmarks) {
-    //   drawConnectors(canvasCtx, results.faceLandmarks, FACEMESH_TESSELATION, { color: "#C0C0C070", lineWidth: 1 });
-    // }
-
-    // // Draw left hand landmarks
-    // if (results.leftHandLandmarks) {
-    //   drawConnectors(canvasCtx, results.leftHandLandmarks, HAND_CONNECTIONS, { color: "#CC0000", lineWidth: 5 });
-    // }
-
-    // // Draw right hand landmarks
-    // if (results.rightHandLandmarks) {
-    //   drawConnectors(canvasCtx, results.rightHandLandmarks, HAND_CONNECTIONS, { color: "#00CC00", lineWidth: 5 });
-    // }
-    canvasCtx.restore();
-  }
+  //     const boundingRect = calcBoundingRect(
+  //       imageWidth,
+  //       imageHeight,
+  //       results.leftHandLandmarks
+  //     );
+  //     const preProcessedLandmarks = preProcessLandmark(
+  //       results.leftHandLandmarks.landmark
+  //     );
+  //     console.log(boundingRect);
+  //     console.log(preProcessedLandmarks);
+  //   }
+  //   if (results.poseLandmarks) {
+  //     const preProcessedLandmarksFace = preProcessLandmark(
+  //       results.poseLandmarks.landmark
+  //     );
+  //     console.log(preProcessedLandmarksFace);
+  //   }
+      canvasCtx.restore();
+  }}
 
   useEffect(() => {
-    const holisticEffect = new Holistic({
+    const holistic = new Holistic({
       locateFile: (file) => {
         return `https://cdn.jsdelivr.net/npm/@mediapipe/holistic/${file}`;
       },
     });
 
-    holisticEffect.setOptions({
+    holistic.setOptions({
       modelComplexity: 1,
       smoothLandmarks: true,
       enableSegmentation: false,
@@ -71,21 +160,22 @@ function App() {
       minTrackingConfidence: 0.5,
     });
 
-    holisticEffect.onResults(onResults);
+    holistic.onResults(onResults);
 
     if (webcamRef.current !== null) {
-      cameraRef.current = new cam.Camera(webcamRef.current.video, {
+      cameraRef.current = new HolisticModule.Camera(webcamRef.current.video, {
         onFrame: async () => {
-          await holisticEffect.send({ image: webcamRef.current.video });
+          await holistic.send({ image: webcamRef.current.video });
         },
         width: 640,
         height: 480,
       });
       cameraRef.current.start();
     }
+
   });
 
-  return (
+  return  (
     <div className="App">
       <header className="App-header">
         <center>
@@ -132,5 +222,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
